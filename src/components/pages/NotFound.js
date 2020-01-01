45c383a998e9f71c36980030c7d7d8070728f46b
Abdul-fatai <staticdev20046@gmail.com>
2020-01-01 03:36:44 +0100

    navbar updated

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -3,7 +3,8 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div className="card text-center">
-      <h1>Page Not Found</h1>
+      <h1>404</h1>
+      <h3>Page Not Found</h3>
       <p className="lead">The page you are looking for does not exits...</p>
     </div>
   );
