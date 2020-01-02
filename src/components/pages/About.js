65c7a16882864eb21f05a5dc7d523ff20cd63f29
Abdul-fatai <staticdev20046@gmail.com>
2020-01-02 10:55:25 +0100

    made some code clean up

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,9 +3,11 @@ import React from 'react';
 const About = () => {
   return (
     <React.Fragment>
-      <h1>About This App</h1>
-      <p>App to search Github users</p>
-      <p>Version: 1.0.0</p>
+      <div className="card text-center">
+        <h1>About This App</h1>
+        <p>App to search Github users</p>
+        <p>Version: 1.0.0</p>
+      </div>
     </React.Fragment>
   );
 };
